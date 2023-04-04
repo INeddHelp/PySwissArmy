205e59cef097ca00d40924b193b40de7f78f5468
INeedHelp <leonardo.bartolelli@gmail.com>
2023-04-04 20:21:15 +0200

    v0.1.0

---
+++ pyswissarmy/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from .ToolboxPy import *
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='PySwissArmy',
+    version='0.1.0',
+    description='PyBox is a Python utility library designed to make common programming tasks easier.',
+    author='INeddHelp',
+    author_email='ineddhelpgithub@gmail.com',
+    packages=['pyswissarmy']
+)
