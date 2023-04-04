f84d3e1ad6341aca506298206c6bbe4ea6d0134b
INeedHelp <leonardo.bartolelli@gmail.com>
2023-04-04 20:25:00 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='PySwissArmy',
-    version='0.1.0',
-    description='PyBox is a Python utility library designed to make common programming tasks easier.',
+    version='0.1.1',
+    description='PySwissArmy is a Python utility library designed to make common programming tasks easier.',
     author='INeddHelp',
     author_email='ineddhelpgithub@gmail.com',
     packages=['pyswissarmy']
