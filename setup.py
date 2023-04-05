b0592da63ab9c3067f8e144bd3a2925eb5bf8cb2
INeedHelp <leonardo.bartolelli@gmail.com>
2023-04-05 15:35:12 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PySwissArmy',
-    version='0.1.1',
+    version='0.1.2',
     description='PySwissArmy is a Python utility library designed to make common programming tasks easier.',
     author='INeddHelp',
     author_email='ineddhelpgithub@gmail.com',
-    packages=['pyswissarmy']
+    packages=find_packages(),
+    install_requires=[
+        'pandas'
+    ],
 )
